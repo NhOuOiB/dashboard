dcaed31e68b336e52a22a58790e7d51866dfeee9
mike.chang <s33165309000@gmail.com>
2024-03-14 17:55:38 +0800

    [feat] dashboard layout

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,16 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+'use client';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import React from 'react';
 
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "Currecy State",
-  description: "Check your assets in any time",
-};
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <React.StrictMode>
+        <html lang="en">
+          <body className={inter.className}>{children}</body>
+        </html>
+    </React.StrictMode>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,46 +1,49 @@
-import Image from 'next/image';
+'use client';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+
   return (
     <main className="w-full h-screen flex flex-col">
+
       <div className="flex-grow p-1 pb-0 flex">
         <div className="flex-grow flex flex-col">
           <div className="flex-grow pb-1">
-            <div className="w-full h-full bg-slate-500"></div>
+            <div className="w-full h-full bg-slate-800"></div>
           </div>
           <div className="flex-grow">
-            <div className="w-full h-full bg-slate-600"></div>
+            <div className="w-full h-full bg-slate-800"></div>
           </div>
         </div>
         <div className="w-1/2 flex flex-col px-1">
           <div className="h-36 flex pb-1">
             <div className="flex-grow ">
-              <div className="w-full h-full bg-slate-500"></div>
+              <div className="w-full h-full bg-slate-800"></div>
             </div>
             <div className="flex-grow px-1 ">
-              <div className="w-full h-full bg-slate-500"></div>
+              <div className="w-full h-full bg-slate-800"></div>
             </div>
             <div className="flex-grow">
-              <div className="w-full h-full bg-slate-500"></div>
+              <div className="w-full h-full bg-slate-800"></div>
             </div>
           </div>
-          <div className="flex-grow bg-slate-600"></div>
+          <div className="flex-grow bg-slate-800"></div>
         </div>
         <div className="flex-grow flex flex-col">
           <div className="flex-grow pb-1">
-            <div className="w-full h-full bg-slate-500"></div>
+            <div className="w-full h-full bg-slate-800"></div>
           </div>
           <div className="h-72">
-            <div className="w-full h-full bg-slate-600"></div>
+            <div className="w-full h-full bg-slate-800"></div>
           </div>
         </div>
       </div>
       <div className="w-full min-h-64 p-1 flex ">
         <div className="flex-grow pr-1 ">
-          <div className="w-full h-full bg-slate-200"></div>
+          <div className="w-full h-full bg-slate-800"></div>
         </div>
         <div className="flex-grow">
-          <div className="w-full h-full bg-slate-400"></div>
+          <div className="w-full h-full bg-slate-800"></div>
         </div>
       </div>
     </main>
